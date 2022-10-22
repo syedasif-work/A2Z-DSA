d1d6bdcc9fbfd19eab2ce7d369c11ba2c8c0e4ef
Syed Asif <syedasn@amazon.com>
2022-10-22 15:39:47 +0530

    Pattern3 to Pattern6 Completed

---
+++ Basics/Patterns/Pattern3.java
@@ -0,0 +1,33 @@
+// Geek is very fond of patterns. Once, his teacher gave him a  pattern to solve. He gave Geek an integer n and asked him to build a pattern.
+
+// Help Geek to build a pattern.
+
+// Example 1:
+
+// Input: 5
+
+// Output:
+// 1
+// 1 2 
+// 1 2 3 
+// 1 2 3 4 
+// 1 2 3 4 5
+import java.util.*;
+
+public class Pattern3 {
+
+    static void printTriangle(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printTriangle(n);
+    }
+}
